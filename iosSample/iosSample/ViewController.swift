2c6a12e87cc4f1b73067d558e97e38d84ab5665f
Artur Babichev <artik222012@gmail.com>
2022-05-16 06:10:31 +0500

    create xcode proj

---
+++ iosSample/iosSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSample
+//
+//  Created by Artur Babichev on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
