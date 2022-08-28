410014c88ede0bc002950033ba654fa0ededbb07
Artur Babichev <artik222012@gmail.com>
2022-08-28 18:03:48 +0400

    add remaining ios targets

--- sample-ios/build.gradle.kts
+++ sample-ios/build.gradle.kts
@@ -4,19 +4,23 @@ plugins {
     id("dev.icerock.mobile.multiplatform-resources")
 }
 
-kotlin {
-    iosX64("uikitX64") {
-        binaries {
-            executable {
-                entryPoint = "com.softartdev.sample.main"
-                freeCompilerArgs += listOf(
-                    "-linker-option", "-framework", "-linker-option", "Metal",
-                    "-linker-option", "-framework", "-linker-option", "CoreText",
-                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
-                )
-            }
+val binConfig: org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget.() -> Unit = {
+    binaries {
+        executable {
+            entryPoint = "com.softartdev.sample.main"
+            freeCompilerArgs += listOf(
+                "-linker-option", "-framework", "-linker-option", "Metal",
+                "-linker-option", "-framework", "-linker-option", "CoreText",
+                "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+            )
         }
     }
+}
+
+kotlin {
+    iosX64("uikitX64", binConfig)
+    iosArm64("uikitArm64", binConfig)
+    iosSimulatorArm64("uikitSimulatorArm64", binConfig)
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -27,6 +31,17 @@ kotlin {
                 implementation(compose.material)
                 implementation(compose.runtime)
                 implementation("dev.icerock.moko:resources:${rootProject.extra["moko_resources_version"]}")
+            }
+        }
+        val uikitX64Main by getting
+        val uikitArm64Main by getting
+        val uikitSimulatorArm64Main by getting
+        val uikitMain by creating {
+            dependsOn(commonMain)
+            uikitX64Main.dependsOn(this)
+            uikitArm64Main.dependsOn(this)
+            uikitSimulatorArm64Main.dependsOn(this)
+            dependencies {
             }
         }
     }
