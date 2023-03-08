9e762f8af8544084bf9a0057227bb2fb1ca3333b
Artur Babichev <artik222012@gmail.com>
2023-03-08 18:51:03 +0400

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.3.1-rc01")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.3.1-rc02")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
     }
 }
