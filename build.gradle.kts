60c8471b38ddff8c9ea402c5d731f337179e1741
Artur Babichev <artik222012@gmail.com>
2023-08-12 17:48:09 +0400

    kotlin 1.8.22, compose 1.5.0-beta02, androidSDK 34

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
         classpath("com.android.tools.build:gradle:8.0.2")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.5.0-beta02")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
