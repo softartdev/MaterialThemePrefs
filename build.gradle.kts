c2ab6eadadcbae0437cf4a1d5103348c3ace3e11
Artur Babichev <artik222012@gmail.com>
2024-01-27 04:00:23 +0400

    kotlin 1.9.22, compose 1.6.0-beta01

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.5.11")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.0-beta01")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
     }
 }
