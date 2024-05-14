d2650b3cf8dbea82d6295ae6cd125bcf17e6d569
babichev.a <babichev.a@promail366.com>
2024-05-14 22:03:40 +0400

    kotlin 2.0.0-RC3, compose 1.6.10-rc02

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    extra["kotlin_version"] = "2.0.0-RC2"
+    extra["kotlin_version"] = "2.0.0-RC3"
     extra["android_min_sdk_version"] = 21
     extra["android_compile_sdk_version"] = 34
     extra["jdk_version"] = 17
@@ -13,7 +13,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
         classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${rootProject.extra["kotlin_version"]}")
         classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.10-rc01")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.10-rc02")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
     }
 }
