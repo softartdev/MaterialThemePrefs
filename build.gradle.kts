7c2bf95c8a1f8c55fb9f82189fd4406462be43b0
Artur Babichev <artik222012@gmail.com>
2024-05-21 21:41:49 +0400

    kotlin 2.0.0, compose 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    extra["kotlin_version"] = "2.0.0-RC3"
+    extra["kotlin_version"] = "2.0.0"
     extra["android_min_sdk_version"] = 21
     extra["android_compile_sdk_version"] = 34
     extra["jdk_version"] = 17
@@ -12,8 +12,8 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
         classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${rootProject.extra["kotlin_version"]}")
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.10-rc03")
+        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.10")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
     }
 }
