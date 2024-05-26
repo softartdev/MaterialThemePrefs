e75de3fea9d87f19f2f855603a5846c3f08cbd9d
Artur Babichev <artik222012@gmail.com>
2024-05-26 22:19:10 +0400

    moko-resources 0.24.0-beta-5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     extra["android_min_sdk_version"] = 21
     extra["android_compile_sdk_version"] = 34
     extra["jdk_version"] = 17
-    extra["moko_resources_version"] = "0.24.0-beta-4"
+    extra["moko_resources_version"] = "0.24.0-beta-5"
     repositories {
         gradlePluginPortal()
         google()
