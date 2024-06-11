d95ac9969783b12a3308026a5f67ab5f20a621eb
Artur Babichev <artik222012@gmail.com>
2024-06-12 03:41:06 +0400

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     extra["android_min_sdk_version"] = 21
     extra["android_compile_sdk_version"] = 34
     extra["jdk_version"] = 17
-    extra["moko_resources_version"] = "0.24.0-beta-5"
+    extra["moko_resources_version"] = "0.24.0"
     repositories {
         gradlePluginPortal()
         google()
@@ -11,8 +11,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
-        classpath("com.android.tools.build:gradle:8.4.1")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.11")
         classpath("dev.icerock.moko:resources-generator:${rootProject.extra["moko_resources_version"]}")
     }
 }
