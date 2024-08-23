75fe9e8e673e2b820931166285876f173dd5085f
Artur Babichev <artik222012@gmail.com>
2024-08-23 11:18:16 +0400

    kotlin 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    extra["kotlin_version"] = "2.0.10"
+    extra["kotlin_version"] = "2.0.20"
     extra["android_min_sdk_version"] = 21
     extra["android_compile_sdk_version"] = 34
     extra["jdk_version"] = 17
@@ -11,7 +11,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
         classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${rootProject.extra["kotlin_version"]}")
-        classpath("com.android.tools.build:gradle:8.5.1")
+        classpath("com.android.tools.build:gradle:8.5.2")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.11")
     }
 }
